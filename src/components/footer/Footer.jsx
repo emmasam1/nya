9ae93264a56d6ed0f81c89213e257a6a0f11d8e9
Emmanuel <findm4@gmail.com>
2024-07-04 11:26:41 +0100

    home

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,207 +15,224 @@ const Footer = () => {
 
   return (
     <div className="bg-[#FFF9EA]">
-        <div className="m-auto mt-20 w-11/12">
+      <div className="m-auto mt-20 w-11/12">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-      <div className="p-10 col-span-1">
-        <h2 className="mb-10 text-2xl font-semibold">Join Us</h2>
-        <Form
-          form={form}
-          layout="vertical"
-          initialValues={{
-            requiredMarkValue: requiredMark,
-          }}
-          onValuesChange={onRequiredTypeChange}
-        >
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              {
-                type: "email",
-                message: "The input is not a valid email!",
-              },
-              {
-                required: true,
-                message: "Please input your email!",
-              },
-            ]}
-          >
-            <Input placeholder="Email" />
-          </Form.Item>
-          <Form.Item
-            label="Phone"
-            name="phone"
-            rules={[
-              {
-                required: true,
-                message: "Please input your phone number!",
-              },
-            ]}
-          >
-            <Input placeholder="Phone" />
-          </Form.Item>
-          <div className="mb-3">
-            <p>
-              <b>Sign up to get texts.</b> By entering your phone number, you
-              are consenting to receive recurring automated text messages &
-              calls from the DNC with news & info about our work. For SMS,
-              message & data rates may apply. Text STOP to end. Text HELP for
-              help.
-            </p>
-          </div>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
-      <div className="col-span-2 p-10 relative">
-        <div className="bg-img h-full absolute w-full top-0 left-0"></div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 border-b-2 pb-5">
-          <div>
-            <Link
-              to="#"
-              className="footer-link relative font-semibold hover:text-green-700"
+          <div className="p-10 col-span-1">
+            <h2 className="mb-10 text-3xl font-bold text-green-800">Join Us</h2>
+            <Form
+              form={form}
+              layout="vertical"
+              initialValues={{
+                requiredMarkValue: requiredMark,
+              }}
+              onValuesChange={onRequiredTypeChange}
+              onFinish={(values) => {
+                console.log("Form values: ", values);
+              }}
             >
-              Who we are
-            </Link>
-            <ul className="mt-4">
-              <li className="mb-4">
-                <Link
-                  to=""
-                  className="footer-link relative text-[.8rem] hover:text-green-700"
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                  {
+                    type: "email",
+                    message: "The input is not a valid email!",
+                  },
+                  {
+                    required: true,
+                    message: "Please input your email!",
+                  },
+                ]}
+              >
+                <Input placeholder="Email" />
+              </Form.Item>
+              <Form.Item
+                label="Phone"
+                name="phone"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your phone number!",
+                  },
+                ]}
+              >
+                <Input placeholder="Phone" />
+              </Form.Item>
+              <div className="mb-3">
+                <p className="font-semibold">
+                  <b>Sign up to get texts.</b> By entering your phone number, you
+                  are consenting to receive recurring automated text messages &
+                  calls from the DNC with news & info about our work. For SMS,
+                  message & data rates may apply. Text STOP to end. Text HELP for
+                  help.
+                </p>
+              </div>
+              <Form.Item>
+                <button
+                  htmlType="submit"
+                  className="border-red-500 text-red-500 border-2 px-6 py-3 font-bold rounded hover:bg-red-500 hover:text-white transition-all duration-300 ease-in"
                 >
-                  What we do
-                </Link>
-              </li>
-              <li className="mb-4">
-                <Link
-                  to=""
-                  className="footer-link relative text-[.8rem] hover:text-green-700"
-                >
-                  Who we serve
-                </Link>
-              </li>
-              <li className="mb-4">
-                <Link
-                  to=""
-                  className="footer-link relative text-[.8rem] hover:text-green-700"
-                >
-                  Our history
-                </Link>
-              </li>
-              <li className="mb-4">
-                <Link
-                  to=""
-                  className="footer-link relative text-[.8rem] hover:text-green-700"
-                >
-                  Leadership
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to=""
-                  className="footer-link relative text-[.8rem] hover:text-green-700"
-                >
-                  State Parties
-                </Link>
-              </li>
-            </ul>
+                  Submit
+                </button>
+              </Form.Item>
+            </Form>
           </div>
-          <div>
-            <Link
-              to="#"
-              className="footer-link relative font-semibold hover:text-green-700"
-            >
-              Where we stand
-            </Link>
-            <ul className="mt-4">
-              <li className="mb-4">
+          <div className="col-span-2 p-10 relative">
+            <div className="bg-img h-full absolute w-full top-0 left-0"></div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 border-b-2 pb-5">
+              <div>
                 <Link
-                  to=""
-                  className="footer-link relative text-[.8rem] hover:text-green-700"
+                  to="#"
+                  className="footer-link relative font-bold hover:text-green-700"
                 >
-                  Party platform
+                  Who we are
                 </Link>
-              </li>
-              <li className="mb-4">
+                <ul className="mt-4">
+                  <li className="mb-4">
+                    <Link
+                      to=""
+                      className="footer-link relative font-semibold text-[.8rem] hover:text-green-700"
+                    >
+                      What we do
+                    </Link>
+                  </li>
+                  <li className="mb-4">
+                    <Link
+                      to=""
+                      className="footer-link relative font-semibold text-[.8rem] hover:text-green-700"
+                    >
+                      Who we serve
+                    </Link>
+                  </li>
+                  <li className="mb-4">
+                    <Link
+                      to=""
+                      className="footer-link relative font-semibold text-[.8rem] hover:text-green-700"
+                    >
+                      Our history
+                    </Link>
+                  </li>
+                  <li className="mb-4">
+                    <Link
+                      to=""
+                      className="footer-link relative font-semibold text-[.8rem] hover:text-green-700"
+                    >
+                      Leadership
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to=""
+                      className="footer-link relative font-semibold text-[.8rem] hover:text-green-700"
+                    >
+                      State Parties
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div>
                 <Link
-                  to=""
-                  className="footer-link relative text-[.8rem] hover:text-green-700"
+                  to="#"
+                  className="footer-link relative font-bold hover:text-green-700"
                 >
-                  The issues
+                  Where we stand
                 </Link>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <Link
-              to="#"
-              className="footer-link relative font-semibold hover:text-green-700"
-            >
-              Take action
-            </Link>
-            <ul className="mt-4">
-              <li className="mb-4">
+                <ul className="mt-4">
+                  <li className="mb-4">
+                    <Link
+                      to=""
+                      className="footer-link relative font-semibold text-[.8rem] hover:text-green-700"
+                    >
+                      Party platform
+                    </Link>
+                  </li>
+                  <li className="mb-4">
+                    <Link
+                      to=""
+                      className="footer-link relative font-semibold text-[.8rem] hover:text-green-700"
+                    >
+                      The issues
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div>
                 <Link
-                  to=""
-                  className="footer-link relative text-[.8rem] hover:text-green-700"
+                  to="#"
+                  className="footer-link relative font-bold hover:text-green-700"
                 >
-                  Events
+                  Take action
                 </Link>
-              </li>
-              <li className="mb-4">
-                <Link
-                  to=""
-                  className="footer-link relative text-[.8rem] hover:text-green-700"
-                >
-                  Work with us
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+                <ul className="mt-4">
+                  <li className="mb-4">
+                    <Link
+                      to=""
+                      className="footer-link relative font-semibold text-[.8rem] hover:text-green-700"
+                    >
+                      Events
+                    </Link>
+                  </li>
+                  <li className="mb-4">
+                    <Link
+                      to=""
+                      className="footer-link relative font-semibold text-[.8rem] hover:text-green-700"
+                    >
+                      Work with us
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
 
-        <div className="mt-5 flex justify-between">
-          <Link
-            to=""
-            className="footer-link relative text-[.8rem] hover:text-green-700"
-          >
-            Work with us
-          </Link>
-          <Link
-            to=""
-            className="footer-link relative text-[.8rem] hover:text-green-700"
-          >
-            Press releases
-          </Link>
-          <Link
-            to=""
-            className="footer-link relative text-[.8rem] hover:text-green-700"
-          >
-            Privacy policy
-          </Link>
-          <Link
-            to=""
-            className="footer-link relative text-[.8rem] hover:text-green-700"
-          >
-            Terms of service
-          </Link>
-        </div>
+            <div className="mt-5 flex justify-between">
+              <Link
+                to=""
+                className="footer-link relative font-semibold text-[.8rem] hover:text-green-700"
+              >
+                Work with us
+              </Link>
+              <Link
+                to=""
+                className="footer-link relative font-semibold text-[.8rem] hover:text-green-700"
+              >
+                Press releases
+              </Link>
+              <Link
+                to=""
+                className="footer-link relative font-semibold text-[.8rem] hover:text-green-700"
+              >
+                Privacy policy
+              </Link>
+              <Link
+                to=""
+                className="footer-link relative font-semibold text-[.8rem] hover:text-green-700"
+              >
+                Terms of service
+              </Link>
+            </div>
 
-        <div className="mt-8">
-            <p className="text-[.7rem] mb-2 font-mono">Copyright &copy; {currentYear} NYA Services Corporation All rights reserved.</p>
-            <p className="text-[.7rem] mb-2 font-mono">Paid for by the DEMOCRATIC NATIONAL COMMITTEE (202) 863-8000</p>
-            <p className="text-[.7rem] mb-2 font-mono">This communication is not authorized by any candidate or candidate's committee.</p>
-        </div>
-        <div className="mt-8">
-            <p className="text-[.7rem] mb-2 font-mono">House 19, 44 Crescent CITEC Villa Estate Off<br/> 6th Avenue Gwarimpa, Abuja, Nigeria <br/> 0807-370-3001</p>
+            <div className="mt-8">
+              <p className="text-[.7rem] mb-2 font-mono">
+                Copyright &copy; {currentYear} NYA Services Corporation All rights
+                reserved.
+              </p>
+              <p className="text-[.7rem] mb-2 font-mono">
+                Paid for by the NATIONAL YOUTH ALLIANCE 0807-370-3001
+              </p>
+              <p className="text-[.7rem] mb-2 font-mono">
+                This communication is not authorized by any candidate or
+                candidate's committee.
+              </p>
+            </div>
+            <div className="mt-8">
+              <p className="text-[.7rem] mb-2 font-mono">
+                House 19, 44 Crescent CITEC Villa Estate Off
+                <br /> 6th Avenue Gwarimpa, Abuja, Nigeria <br /> 0807-370-3001
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,65 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
 import Banner from "../components/banner/Banner";
 import { Card } from "antd";
+import image1 from '../assets/images/img_13.jpg';
+import image2 from '../assets/images/img_5.jpg';
+import image3 from '../assets/images/img_3.jpg';
+
 const { Meta } = Card;
 
 const Home = () => {
   const data = [
     {
       id: 1,
-      image:
-        "https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true",
+      image: image1,
       title: "Preparing and folding laundry",
       content:
         "Our caregivers are trained to efficiently handle laundry tasks, including washing, drying, folding, and putting away clothes. We ensure that your loved one's clothes are clean, fresh, and well-organized.",
     },
     {
       id: 2,
-      image:
-        "https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true",
+      image:image2,
       title: "Taking out the garbage as directed",
       content:
         "Our caregivers can assist with regular garbage disposal according to your specific instructions. Whether it's daily, weekly, or as needed, we ensure that your home remains clean and sanitary.",
     },
     {
       id: 3,
-      image:
-        "https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true",
+      image:image3,
       title: "Home organization and management",
       content:
         "We help maintain a clutter-free and organized home environment. From arranging items to keeping spaces tidy, our caregivers ensure that everything is in its place.",
-    },
-    {
-      id: 4,
-      image:
-        "https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true",
-      title: "General Cleaning",
-      content:
-        "Our caregivers can handle various cleaning tasks, such as dusting, vacuuming, mopping, and cleaning bathrooms and kitchens. We strive to create a clean and comfortable living space for your loved one.",
-    },
-    {
-      id: 5,
-      image:
-        "https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true",
-      title: "Cooking nutritious meals",
-      content:
-        "We provide meal preparation services tailored to your loved one's dietary needs and preferences. Our caregivers ensure that your loved one receives delicious and nutritious meals every day.",
-    },
-    {
-      id: 6,
-      image:
-        "https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true",
-      title: "Serving it to your comfort",
-      content:
-        "Our caregivers offer assistance with meal service, ensuring that meals are served at the right time and in a manner that meets your loved one's preferences. We aim to make mealtime enjoyable and stress-free.",
-    },
-    {
-      id: 7,
-      image:
-        "https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true",
-      title: "Grocery shopping and general errands ",
-      content:
-        "We can help with grocery shopping and running general errands, ensuring that your loved one has everything they need. Our caregivers can also assist with picking up prescriptions and other essential items.",
     },
   ];
 
@@ -70,21 +41,25 @@ const Home = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
           {data.map((e) => (
             <Card
-              id={e.id}
+              key={e.id}
               hoverable
-              style={
-                {
-                  //   width: 300,
-                  // height: 400,
-                }
-              }
-              cover={<img alt="example" src={e.image} />}
+              className="custom-card" // Apply the custom class
+              cover={<img alt="example" src={e.image} style={{ height: 250, objectFit: 'cover' }} />}
             >
               <Meta
                 title={e.title}
                 description={e.content}
                 className="md:pb-5 sm:pb-5 pb-5"
               />
+              <Link
+                to="/"
+                smooth
+                duration={500}
+                className="group font-medium text-green-800 hover:text-red-600 inline-flex items-center py-1"
+              >
+                Read more
+                <FaArrowRight className="mt-1 ml-2 transition-transform transform group-hover:rotate-90" />
+              </Link>
             </Card>
           ))}
         </div>
